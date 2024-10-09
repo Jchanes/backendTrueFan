74ffb64b7793fbc3791ae8ff3480045b4faa82ca
EH Islas <eduardohuertaislas96@gmail.com>
2024-10-09 15:29:13 -0600

    Se agrega class ProductoController

---
+++ dataBase/src/main/java/org/trueFanBoutique/dataBase/service/ProductoService.java
@@ -0,0 +1,35 @@
+package org.trueFanBoutique.dataBase.service;
+
+import java.util.List;
+
+import org.trueFanBoutique.dataBase.model.Producto;
+
+public class ProductoService {
+
+	public List<Producto> getAllProductos() {
+		
+		return null;
+	}
+
+	public Producto getProducto(Long prodId) {
+		
+		return null;
+	}
+
+	public Producto addProducto(Producto producto) {
+	
+		return null;
+	}
+
+	public Producto deleteProducto(Long prodId) {
+	
+		return null;
+	}
+
+	public Producto updateProducto(Long prodId, String genero, String nombre, String descripcion, String imagen,
+			Double precio) {
+
+		return null;
+	}
+
+}
